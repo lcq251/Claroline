ca6c1076fd4e80e20c1835259f5fe0f054ddf25d
Elorfin <penin.axel@gmail.com>
2024-10-31 12:22:00 +0100

    [Dev] fixes translation cmd DI

--- src/main/dev/DependencyInjection/ClarolineDevExtension.php
+++ src/main/dev/DependencyInjection/ClarolineDevExtension.php
@@ -16,9 +16,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 
-/**
- * Loads the core services configuration files.
- */
 class ClarolineDevExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
