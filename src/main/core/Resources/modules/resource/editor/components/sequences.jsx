24f92cb7de71c6a0d8f3d937357d5f2e39cfa66c
Elorfin <penin.axel@gmail.com>
2025-04-01 08:41:59 +0200

    [Resource] removes obsolete evaluation parameters

--- src/main/core/Resources/modules/resource/editor/components/sequences.jsx
+++ src/main/core/Resources/modules/resource/editor/components/sequences.jsx
@@ -2,40 +2,11 @@ import React from 'react'
 
 import {trans} from '#/main/app/intl'
 import {EditorPage} from '#/main/app/editor'
-import get from 'lodash/get'
 
-const ResourceEditorSequences = (props) =>
+const ResourceEditorSequences = () =>
   <EditorPage
     title={trans('Scénarisation')}
     help={trans('Retrouver tous les scénarios pédagogiques utilisant cette ressource.')}
-    definition={[
-      {
-        title: trans('general'),
-        primary: true,
-        fields: [
-          {
-            name: 'resourceNode.evaluation.required',
-            label: trans('require_resource', {}, 'resource'),
-            type: 'boolean',
-            help: trans('require_resource_help', {}, 'resource'),
-            onChange: (required) => {
-              if (!required) {
-                updateProp('resourceNode.evaluation.evaluated', false)
-              }
-            },
-            linked: [
-              {
-                name: 'resourceNode.evaluation.evaluated',
-                label: trans('evaluate_resource', {}, 'resource'),
-                type: 'boolean',
-                help: trans('evaluate_resource_help', {}, 'resource'),
-                displayed: (resource) => get(resource, 'resourceNode.evaluation.required', false)
-              }
-            ]
-          }
-        ]
-      }
-    ]}
   >
     Liste des Séquences
   </EditorPage>
