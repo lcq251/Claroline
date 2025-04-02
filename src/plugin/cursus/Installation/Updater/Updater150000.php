8f9e5fc27088c523a3dbed7ead03c6f6b46e808b
Elorfin <penin.axel@gmail.com>
2025-04-02 08:55:47 +0200

    [Training] links Courses to default orga if none

--- src/plugin/cursus/Installation/Updater/Updater150000.php
+++ src/plugin/cursus/Installation/Updater/Updater150000.php
@@ -6,32 +6,38 @@ use Claroline\AppBundle\Persistence\ObjectManager;
 use Claroline\CoreBundle\Component\Context\PublicContext;
 use Claroline\CoreBundle\Entity\Organization\Organization;
 use Claroline\CoreBundle\Entity\Tool\OrderedTool;
+use Claroline\CoreBundle\Manager\OrganizationManager;
 use Claroline\InstallationBundle\Updater\Updater;
+use Doctrine\DBAL\Connection;
 
 class Updater150000 extends Updater
 {
-    public function __construct(private readonly ObjectManager $om)
-    {
+    public function __construct(
+        private readonly Connection $connection,
+        private readonly OrganizationManager $organizationManager
+    ) {
     }
 
     public function postUpdate(): void
     {
-        $this->assignCoursesToOrganizations();
+        $this->linkCoursesToOrganizations();
 
         // migrate registered groups to sessions and events
     }
 
-    private function assignCoursesToOrganizations(): void
+    private function linkCoursesToOrganizations(): void
     {
-        $defaultOrganization = $this->om->getRepository(Organization::class)->findOneBy([
-            'default' => true,
-        ]);
+        $organization = $this->organizationManager->getDefault(true);
 
+        $updateQuery = $this->connection->prepare('
+            INSERT INTO claro_cursusbundle_course_organizations (course_id, organization_id)
+            SELECT c.id AS course_id, :default_organization_id AS organization_id
+            FROM claro_cursusbundle_course AS c 
+            LEFT JOIN claro_cursusbundle_course_organizations AS co ON c.id = co.course_id 
+            WHERE co.organization_id IS NULL
+        ');
 
-    }
-
-    private function migrateGroupRegistrations(): void
-    {
-
+        $updateQuery->bindValue('default_organization_id', $organization->getId());
+        $updateQuery->executeQuery();
     }
 }
