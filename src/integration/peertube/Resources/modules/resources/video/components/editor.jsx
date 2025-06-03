a5f4a51e47c88e1edee7ddf4980754c74776126c
Elorfin <penin.axel@gmail.com>
2025-06-03 11:26:45 +0200

    [Peertube] fixes parameters edition

--- src/integration/peertube/Resources/modules/resources/video/components/editor.jsx
+++ src/integration/peertube/Resources/modules/resources/video/components/editor.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
+import get from 'lodash/get'
 
 import {trans} from '#/main/app/intl/translation'
 import {EditorPage} from '#/main/app/editor'
-import {ResourceEditor} from '#/main/core/resource/editor'
+import {actions, ResourceEditor, ResourceEditorOverview} from '#/main/core/resource/editor'
 
 import {selectors} from '#/integration/peertube/resources/video/store/selectors'
 
-const VideoEditorParameters = () =>
-  <EditorPage
-    title={trans('parameters')}
+const VideoEditorOverview = () =>
+  <ResourceEditorOverview
     definition={[
       {
         title: trans('general'),
@@ -17,51 +17,102 @@ const VideoEditorParameters = () =>
         hideTitle: true,
         fields: [
           {
-            name: 'url',
+            name: 'resource.url',
             label: trans('url'),
             type: 'url',
             required: true
           }
         ]
-      }, {
-        icon: 'fa fa-fw fa-play',
-        title: trans('playback_param', {}, 'peertube'),
-        help: trans('timecode_help',{}, 'peertube'),
-        primary: true,
-        fields: [
-          {
-            name: 'timecodeStart',
-            label: trans('timecode_start', {}, 'peertube'),
-            type: 'time'
-          }, {
-            name: 'timecodeEnd',
-            label: trans('timecode_end', {}, 'peertube'),
-            type: 'time'
-          }, {
-            name: 'autoplay',
-            label: trans('autoplay', {}, 'peertube'),
-            type: 'boolean'
-          }, {
-            name: 'looping',
-            label: trans('loop', {}, 'peertube'),
-            type: 'boolean'
-          }, {
-            name: 'controls',
-            label: trans('controls', {}, 'peertube'),
-            type: 'boolean'
-          }, {
-            name: 'peertubeLink',
-            label: trans('peertubeLink', {}, 'peertube'),
-            type: 'boolean'
-          }, {
-            name: 'resume',
-            label: trans('resume', {}, 'peertube'),
-            type: 'boolean'
-          }
-        ]
       }
     ]}
   />
+
+const VideoEditorPlayback = () => {
+  const dispatch = useDispatch()
+
+  return (
+    <EditorPage
+      title={trans('playback')}
+      help={trans('playback_help', {}, 'youtube')}
+      dataPart="resource"
+      definition={[
+        {
+          title: trans('general'),
+          primary: true,
+          hideTitle: true,
+          fields: [
+            {
+              name: 'autoplay',
+              label: trans('autoplay', {}, 'peertube'),
+              help: trans('autoplay_help', {}, 'peertube'),
+              type: 'boolean'
+            }, {
+              name: 'looping',
+              label: trans('loop', {}, 'peertube'),
+              help: trans('loop_help', {}, 'peertube'),
+              type: 'boolean'
+            }, {
+              name: 'controls',
+              label: trans('controls', {}, 'peertube'),
+              help: trans('controls_help', {}, 'peertube'),
+              type: 'boolean'
+            }, {
+              name: 'resume',
+              label: trans('resume', {}, 'peertube'),
+              help: trans('resume_help', {}, 'peertube'),
+              type: 'boolean'
+            }
+          ]
+        }, {
+          title: trans('more'),
+          primary: true,
+          hideTitle: true,
+          fields: [
+            {
+              name: '_timecodeStart',
+              type: 'boolean',
+              label: trans('timecode_start_help', {}, 'peertube'),
+              help: trans('timecode_help',{}, 'peertube'),
+              onChange: (checked) => {
+                if (!checked) {
+                  dispatch(actions.updateResource(null, 'timecodeStart'))
+                }
+              },
+              linked: [
+                {
+                  name: 'timecodeStart',
+                  label: trans('timecode_start', {}, 'peertube'),
+                  type: 'time',
+                  required: true,
+                  displayed: (data) => get(data, '_timecodeStart') || !!get(data, 'timecodeStart')
+                },
+              ]
+            }, {
+              name: '_timecodeEnd',
+              type: 'boolean',
+              label: trans('timecode_end_help', {}, 'peertube'),
+              help: trans('timecode_help',{}, 'peertube'),
+              onChange: (checked) => {
+                if (!checked) {
+                  dispatch(actions.updateResource(null, 'timecodeEnd'))
+                }
+              },
+              linked: [
+                {
+                  name: 'timecodeEnd',
+                  label: trans('timecode_end', {}, 'peertube'),
+                  type: 'time',
+                  required: true,
+                  displayed: (data) => get(data, '_timecodeEnd') || !!get(data, 'timecodeEnd')
+                },
+              ]
+            }
+          ]
+        }
+      ]}
+    />
+  )
+}
 
 const VideoEditor = () => {
   const video = useSelector(selectors.video)
@@ -71,11 +122,12 @@ const VideoEditor = () => {
       additionalData={() => ({
         resource: video
       })}
+      overviewPage={VideoEditorOverview}
       pages={[
         {
-          name: 'parameters',
-          title: trans('parameters'),
-          component: VideoEditorParameters
+          name: 'playback',
+          title: trans('playback'),
+          component: VideoEditorPlayback
         }
       ]}
     />
