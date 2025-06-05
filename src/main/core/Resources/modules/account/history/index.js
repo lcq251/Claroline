94b8f890cdb1ac24123fa7dbc9ac573a4dbad901
Elorfin <penin.axel@gmail.com>
2025-06-05 09:14:42 +0200

    [Account] renames history page in editor

--- src/main/core/Resources/modules/account/history/index.js
+++ src/main/core/Resources/modules/account/history/index.js
@@ -4,7 +4,7 @@ import {declareAccount} from '#/main/community/user'
 import {AccountHistory} from '#/main/core/account/history/components/main'
 
 export default declareAccount(AccountHistory, {
-  name: 'my-history', // ATTENTION : UserEditor contains a history page for operational logs
-  title: trans('history', {}, 'history'),
+  name: 'my-history', // ATTENTION: UserEditor contains a history page for operational logs
+  title: trans('history', {}, 'workspace'),
   group: trans('preferences')
 })
