3fa0703085b889fa1bd3f2ea1e1c65c59868f12d
Elorfin <penin.axel@gmail.com>
2025-06-30 12:43:35 +0200

    [Forum] fixes DataSource

--- src/plugin/forum/Resources/modules/data/components/message-card.jsx
+++ src/plugin/forum/Resources/modules/data/components/message-card.jsx
@@ -3,15 +3,16 @@ import get from 'lodash/get'
 
 import {getPlainText} from '#/main/app/data/types/html/utils'
 import {DataCard} from '#/main/app/data/components/card'
+import {trans} from '#/main/app/intl'
 
 const MessageCard = (props) =>
   <DataCard
     {...props}
     id={props.data.id}
+    asIcon={true}
     poster={get(props.data, 'meta.creator.picture')}
-    icon={get(props.data, 'meta.creator.name') ? props.data.meta.creator.name.charAt(0) : 'fa fa-user'}
-    name={props.data.subject.title}
-    title={props.data.subject.title}
+    name={get(props.data, 'meta.creator.name', trans('unknown'))}
+    title={get(props.data, 'subject.title')}
     contentText={getPlainText(props.data.content)}
   />
 
--- src/plugin/forum/Resources/modules/data/sources/forum-messages.js
+++ src/plugin/forum/Resources/modules/data/sources/forum-messages.js
@@ -4,47 +4,44 @@ import {route} from '#/main/core/resource/routing'
 
 import {MessageCard} from '#/plugin/forum/data/components/message-card'
 
-export default {
-  name: 'forum_messages',
-  parameters: {
-    primaryAction: (message) => ({
-      type: URL_BUTTON,
-      target: `#${route(message.meta.resource)}/subjects/${message.subject.id}`
-    }),
-    definition: [
-      {
-        name: 'content',
-        type: 'html',
-        label: trans('content'),
-        displayed: true,
-        primary: true
-      }, {
-        name: 'subject.title',
-        type: 'string',
-        label: trans('subject', {}, 'forum'),
-        displayed: true
-      }, {
-        name: 'meta.created',
-        alias: 'createdAt',
-        type: 'date',
-        label: trans('date'),
-        displayed: true,
-        options: {time: true}
-      }, {
-        name: 'meta.updated',
-        alias: 'updatedAt',
-        type: 'date',
-        label: trans('last_modification'),
-        displayed: false,
-        options: {time: true}
-      }, {
-        name: 'meta.creator',
-        type: 'user',
-        alias: 'creator',
-        label: trans('creator'),
-        displayed: true
-      }
-    ],
-    card: MessageCard
-  }
-}
+export default () => ({
+  primaryAction: (message) => ({
+    type: URL_BUTTON,
+    target: `#${route(message.meta.resource)}/subjects/${message.subject.id}`
+  }),
+  definition: [
+    {
+      name: 'content',
+      type: 'html',
+      label: trans('content'),
+      displayed: true,
+      primary: true
+    }, {
+      name: 'subject.title',
+      type: 'string',
+      label: trans('subject', {}, 'forum'),
+      displayed: true
+    }, {
+      name: 'meta.created',
+      alias: 'createdAt',
+      type: 'date',
+      label: trans('date'),
+      displayed: true,
+      options: {time: true}
+    }, {
+      name: 'meta.updated',
+      alias: 'updatedAt',
+      type: 'date',
+      label: trans('last_modification'),
+      displayed: false,
+      options: {time: true}
+    }, {
+      name: 'meta.creator',
+      type: 'user',
+      alias: 'creator',
+      label: trans('creator'),
+      displayed: true
+    }
+  ],
+  card: MessageCard
+})
