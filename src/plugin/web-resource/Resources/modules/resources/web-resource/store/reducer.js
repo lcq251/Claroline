c36ac8af126810dc9aea85f2207071a37e2c8948
Elorfin <penin.axel@gmail.com>
2025-07-08 09:01:21 +0200

    [WebResource] fixes store

--- src/plugin/web-resource/Resources/modules/resources/web-resource/store/reducer.js
+++ src/plugin/web-resource/Resources/modules/resources/web-resource/store/reducer.js
@@ -4,7 +4,6 @@ import {combineReducers, makeReducer} from '#/main/app/store/reducer'
 import {RESOURCE_LOAD} from '#/main/core/resource/store'
 
 import {selectors} from '#/plugin/web-resource/resources/web-resource/store/selectors'
-import {reducer as editorReducer} from '#/plugin/web-resource/resources/web-resource/editor/store/reducer'
 
 const reducer = combineReducers({
   path: makeReducer({}, {
@@ -12,8 +11,7 @@ const reducer = combineReducers({
   }),
   file: makeReducer({}, {
     [makeInstanceAction(RESOURCE_LOAD, selectors.STORE_NAME)]: (state, action) => action.resourceData.file
-  }),
-  webResourceForm: editorReducer
+  })
 })
 
 export {
