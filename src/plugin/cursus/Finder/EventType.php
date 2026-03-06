c26f702c103e48769d5f554b8822c307567bb325
Logan <logan.fromont@sinfony.eu>
2026-03-06 09:34:45 +0100

    [Training] fix sort presence by event + add sort by event date start/end (#3051)
    * [CURSUS]: fix sort by event + add sord by event date start/end
    
    * coding style
    
    * coding style

--- src/plugin/cursus/Finder/EventType.php
+++ src/plugin/cursus/Finder/EventType.php
@@ -106,13 +106,6 @@ class EventType extends AbstractType
         ;
     }
 
-    public function buildQuery(QueryBuilder $queryBuilder, FinderInterface $finder, array $options): void
-    {
-        if ($finder->getSortValue()) {
-            $queryBuilder->addOrderBy("{$finder->getQueryPath()}_plannedObject.name", $finder->getSortValue());
-        }
-    }
-
     public function getParent(): ?string
     {
         return EntityType::class;
