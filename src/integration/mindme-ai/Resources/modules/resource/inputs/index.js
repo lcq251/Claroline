12a3f899e222b1d542a1d40033daf909372b976a
liuai <liuai@localhost>
2026-08-13 14:43:24 +0800

    feat: add resource inputs components (editor/display)

---
+++ src/integration/mindme-ai/Resources/modules/resource/inputs/index.js
@@ -0,0 +1,14 @@
+/**
+ * Generic "resource inputs" module.
+ *
+ * Any resource can declare other resources as its inputs through the
+ * ResourceReference API (mindme-ai integration bundle).
+ */
+
+import {ResourceInputsEditor} from '#/integration/mindme-ai/resource/inputs/components/editor'
+import {ResourceInputs} from '#/integration/mindme-ai/resource/inputs/components/display'
+
+export {
+  ResourceInputsEditor,
+  ResourceInputs
+}
