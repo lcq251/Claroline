948d7d0f44978f4bb70f4f648e220c0930e0b169
liuai <liuai@localhost>
2026-08-14 10:57:52 +0800

    feat: add selfRegistration filter to WorkspaceType finder

--- src/main/core/Finder/WorkspaceType.php
+++ src/main/core/Finder/WorkspaceType.php
@@ -37,6 +37,7 @@ class WorkspaceType extends AbstractType
             ->add('description', TextType::class)
             ->add('model', BooleanType::class, ['default' => false])
             ->add('personal', BooleanType::class, ['default' => false])
+            ->add('selfRegistration', BooleanType::class, ['default' => false])
             ->add('hidden', HiddenType::class)
             ->add('archived', BooleanType::class, ['default' => false])
             ->add('public', PublicType::class)
