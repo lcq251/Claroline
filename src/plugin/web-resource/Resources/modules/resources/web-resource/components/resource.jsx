70d19931d29b3c8bc221f0f7358293f81a2e6f6a
liuai <liuai@localhost>
2026-08-14 16:09:10 +0800

    feat: add link-resources action to web-resource toolbar

--- src/plugin/web-resource/Resources/modules/resources/web-resource/components/resource.jsx
+++ src/plugin/web-resource/Resources/modules/resources/web-resource/components/resource.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 
+import {trans} from '#/main/app/intl/translation'
+import {MODAL_BUTTON} from '#/main/app/buttons'
 import {Resource} from '#/main/core/resource'
+import {selectors as resourceSelectors} from '#/main/core/resource'
 
 import {WebResourceEditor} from '#/plugin/web-resource/resources/web-resource/components/editor'
 import {Player} from '#/plugin/web-resource/resources/web-resource/player/components/player'
+import {MODAL_LINK_RESOURCES} from '#/plugin/web-resource/resources/web-resource/modals/inputs'
 
-const WebResource = (props) =>
-  <Resource
-    {...props}
-    editor={WebResourceEditor}
-    pages={[
-      {
-        path: '/',
-        exact: true,
-        component: Player
-      }
-    ]}
-  />
+const WebResource = (props) => {
+  const canEdit = useSelector(resourceSelectors.canEdit)
+  const nodeId = useSelector(resourceSelectors.resourceNode)?.id
+
+  return (
+    <Resource
+      {...props}
+      editor={WebResourceEditor}
+      actions={[
+        {
+          name: 'link-inputs',
+          type: MODAL_BUTTON,
+          icon: 'fa fa-fw fa-link',
+          label: trans('link_resources', {}, 'resource'),
+          displayed: canEdit,
+          modal: [MODAL_LINK_RESOURCES, {hostId: nodeId}]
+        }
+      ]}
+      pages={[
+        {
+          path: '/',
+          exact: true,
+          component: Player
+        }
+      ]}
+    />
+  )
+}
 
 export {
   WebResource
