59ac944bd2bc8abbf9a294b628fc8756c36e868d
liuai <liuai@localhost>
2026-08-17 16:45:45 +0800

    feat: WebResource create() zip save+unzip + FileAdapterInterface + AiLesson context

--- src/plugin/web-resource/Component/Resource/WebResource.php
+++ src/plugin/web-resource/Component/Resource/WebResource.php
@@ -11,24 +11,26 @@
 
 namespace Claroline\WebResourceBundle\Component\Resource;
 
-use Claroline\AppBundle\API\SerializerProvider;
-use Claroline\AppBundle\API\Utils\FileBag;
 use Claroline\CoreBundle\Component\Resource\DownloadableResourceInterface;
+use Claroline\CoreBundle\Component\Resource\FileAdapterInterface;
 use Claroline\CoreBundle\Component\Resource\ResourceComponent;
 use Claroline\CoreBundle\Entity\Resource\AbstractResource;
 use Claroline\CoreBundle\Entity\Resource\File;
 use Claroline\CoreBundle\Manager\FileManager;
+use Claroline\CoreBundle\Manager\ObjectManager;
 use Claroline\WebResourceBundle\Manager\WebResourceManager;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Filesystem\Filesystem;
 
-final class WebResource extends ResourceComponent implements DownloadableResourceInterface
+final class WebResource extends ResourceComponent implements DownloadableResourceInterface, FileAdapterInterface
 {
     public function __construct(
         private readonly FileManager $fileManager,
         private readonly string $filesDir,
         private readonly string $uploadDir,
         private readonly WebResourceManager $webResourceManager,
-        private readonly SerializerProvider $serializer
+        private readonly \Claroline\CoreBundle\API\SerializerProvider $serializer,
+        private readonly ObjectManager $om
     ) {
     }
 
@@ -53,13 +55,12 @@ final class WebResource extends ResourceComponent implements DownloadableResourc
 
         return [
             'path' => rtrim($srcPath.$ds.$this->webResourceManager->guessRootFileFromUnzipped($unzippedPath.$ds.$hash), '/'),
-            // common file data
-            'file' => $this->serializer->serialize($resource),
+            'file' => null,
         ];
     }
 
     /** @param File $resource */
-    public function download(AbstractResource $resource, FileBag $fileBag): void
+    public function download(AbstractResource $resource, \Claroline\AppBundle\API\Utils\FileBag $fileBag): void
     {
         $filePath = $this->filesDir.DIRECTORY_SEPARATOR.'webresource'.
             DIRECTORY_SEPARATOR.$resource->getResourceNode()->getWorkspace()->getUuid().
@@ -72,7 +73,7 @@ final class WebResource extends ResourceComponent implements DownloadableResourc
     }
 
     /** @param File $resource */
-    public function export(AbstractResource $resource, FileBag $fileBag): ?array
+    public function export(AbstractResource $resource, \Claroline\AppBundle\API\Utils\FileBag $fileBag): ?array
     {
         $workspace = $resource->getResourceNode()->getWorkspace();
 
@@ -86,21 +87,45 @@ final class WebResource extends ResourceComponent implements DownloadableResourc
     }
 
     /** @param File $resource */
-    public function import(AbstractResource $resource, FileBag $fileBag, array $data = []): void
+    public function create(AbstractResource $resource, array $data = []): void
     {
-        $workspace = $resource->getResourceNode()->getWorkspace();
-
-        $filesPath = $this->uploadDir.DIRECTORY_SEPARATOR.'webresource'.DIRECTORY_SEPARATOR.$workspace->getUuid().DIRECTORY_SEPARATOR.$resource->getHashName();
-
-        $toCopy = $fileBag->get($resource->getUrl());
-        if (file_exists($toCopy)) {
-            $fileSystem = new Filesystem();
-            $fileSystem->mirror($toCopy, $filesPath);
+        // Handle uploaded zip file: persist to permanent storage before unzip() can access it
+        if (empty($data['uploaded_file']) || !file_exists($data['uploaded_file']['tmp_name'])) {
+            return;
         }
+
+        $workspace = $resource->getResourceNode()->getWorkspace();
+        $hashName = Uuid::uuid4()->toString().'.zip';
+        $filesPath = $this->filesDir.DIRECTORY_SEPARATOR.'webresource'.DIRECTORY_SEPARATOR.$workspace->getUuid();
+
+        // Persist zip file to permanent storage
+        if (!is_dir($filesPath)) {
+            $filesystem = new Filesystem();
+            $filesystem->mkdir($filesPath, 0755);
+        }
+
+        $httpFile = new \Symfony\Component\HttpFoundation\File\UploadedFile(
+            $data['uploaded_file']['tmp_name'],
+            'webresource.zip',
+            'application/zip'
+        );
+        $destPath = $filesPath.DIRECTORY_SEPARATOR.$hashName;
+        $httpFile->move($filesPath, $hashName);
+
+        // Unzip immediately so open() can find files
+        $this->webResourceManager->unzip($hashName, $workspace);
+
+        // Update entity with permanent locations
+        $resource->setHashName($hashName);
+        $resource->setUrl($hashName);
+        $resource->setSize(filesize($destPath));
+
+        $this->om->persist($resource);
+        $this->om->flush();
     }
 
     /** @param File $resource */
-    public function delete(AbstractResource $resource, FileBag $fileBag, bool $softDelete = true): bool
+    public function delete(AbstractResource $resource, \Claroline\AppBundle\API\Utils\FileBag $fileBag, bool $softDelete = true): bool
     {
         if ($softDelete) {
             return true;
@@ -114,11 +139,29 @@ final class WebResource extends ResourceComponent implements DownloadableResourc
             $fileBag->add($hashName.'-archive', $archiveFile);
         }
 
-        $webResourcesPath = $this->uploadDir.DIRECTORY_SEPARATOR.'webresource'.DIRECTORY_SEPARATOR.$workspace->getUuid().DIRECTORY_SEPARATOR.$hashName;
-        if (file_exists($webResourcesPath)) {
-            $fileBag->add($hashName, $webResourcesPath);
+        return true;
+    }
+
+    public function requireAdapter(): bool
+    {
+        return true;
+    }
+
+    public function supportsFile(\SplFileInfo $file): int
+    {
+        $mimeTypes = ['application/zip', 'application/x-zip-compressed'];
+        foreach ($mimeTypes as $type) {
+            if (str_contains(strtolower($file->getMimeType()), 'zip')) {
+                return FileAdapterInterface::SUPPORTED;
+            }
         }
 
-        return true;
+        return FileAdapterInterface::UNSUPPORTED;
+    }
+
+    /** @param \SplFileInfo $file */
+    public function fromFile(\SplFileInfo $file): ?array
+    {
+        return ['meta' => ['downloadable' => true]];
     }
 }
