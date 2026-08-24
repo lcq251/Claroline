ebc12b2e065dfd83c6875cc4ae6789cebd976d43
liuai <liuai@localhost>
2026-08-24 19:32:24 +0800

    feat(player): add table of contents support to markdown player
    Enable TOC (table of contents) in md-editor-rt MdPreview component with level [2, 3] to show h2 and h3 headings

--- src/integration/mindme-ai/Resources/modules/resources/mindme-markdown/components/player.jsx
+++ src/integration/mindme-ai/Resources/modules/resources/mindme-markdown/components/player.jsx
@@ -24,6 +24,7 @@ const MindmeMarkdownPlayer = () => {
                 <MdPreview
                   editorId="mindme-markdown-preview"
                   modelValue={content}
+                  toc={{enable: true, level: [2, 3]}}
                   // previewTheme="github"
                   // codeTheme="github"
                 />
