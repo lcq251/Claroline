e6764f0df03c78ca81c96cb9890a5001237c5616
liuai <liuai@localhost>
2026-08-24 20:05:56 +0800

    feat(player): properly integrate MdCatalog component for table of contents
    Use MdCatalog as separate component with editorId and scrollElement props per md-editor-rt documentation

--- src/integration/mindme-ai/Resources/modules/resources/mindme-markdown/components/player.jsx
+++ src/integration/mindme-ai/Resources/modules/resources/mindme-markdown/components/player.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Suspense} from 'react'
 import {useSelector} from 'react-redux'
 import get from 'lodash/get'
 
@@ -7,32 +7,38 @@ import {PageContent, PageSection, PageSimple} from '#/main/app/page'
 import {ResourcePage, selectors as resourceSelectors} from '#/main/core/resource'
 
 const MdPreview = React.lazy(() => import('md-editor-rt').then(m => ({default: m.MdPreview})))
+const MdCatalog = React.lazy(() => import('md-editor-rt').then(m => ({default: m.MdCatalog})))
 
 const MindmeMarkdownPlayer = () => {
   const resourceNode = useSelector(resourceSelectors.resourceNode)
   const resource = useSelector(resourceSelectors.resource)
 
   const content = get(resource, 'content', '')
+  const [id] = useState('mindme-markdown-preview')
+  const [catalogVisible, setCatalogVisible] = useState(true)
 
   return (
     <ResourcePage>
-      <React.Suspense fallback={<div className="text-center p-5"><span className="fa fa-spinner fa-spin" /></div>}>
+      <Suspense fallback={<div className="text-center p-5"><span className="fa fa-spinner fa-spin" /></div>}>
         <PageSimple>
           <PageContent>
             <PageSection>
               <div className="markdown-preview p-4">
                 <MdPreview
-                  editorId="mindme-markdown-preview"
+                  id={id}
                   modelValue={content}
-                  toc={{enable: true, level: [2, 3]}}
-                  // previewTheme="github"
-                  // codeTheme="github"
+                  catalogVisible={catalogVisible}
                 />
               </div>
+              {catalogVisible && (
+                <div className="markdown-toc">
+                  <MdCatalog editorId={id} scrollElement=".markdown-preview" />
+                </div>
+              )}
             </PageSection>
           </PageContent>
         </PageSimple>
-      </React.Suspense>
+      </Suspense>
     </ResourcePage>
   )
 }
